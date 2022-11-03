d9040408836b3f46499a8ec420668b6df6242721
Bobby Rein <bobbyrein97@gmail.com>
2022-11-02 20:00:23 -0500

    converted context into redux and added Thunk to category in store

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Fully functional and scalable retail website. You can create an account, sign in with google, add items to your cart, and checkout with stripe. 
 
-Built using React.js, Sass, Styled components, and Firebase for authentication.
+Built using React.js 
+Sass and Styled components for styling
+Redux for global state management, Thunk, and persist 
+Firebase for authentication
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from "react";
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from "react-redux";
 
-import CategoriesPreview from '../categories-preview/categories-preview.components';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
+
 
 const Shop = () => {
-    return (
-        <Routes>
-            <Route index element={<CategoriesPreview />} />
-            <Route path=':category' element={<Category />} />
-        </Routes>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategoriesAsync());
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
